b0a939c416a6cf34bf077e51939e168b78302c35
avp02 <vpetranovskij@gmail.com>
2021-09-20 23:12:18 +0300

    create CRUD application method PATCH and DELETE

--- src/main/java/avp/com/spring_mvc_dao/controllers/PeopleController.java
+++ src/main/java/avp/com/spring_mvc_dao/controllers/PeopleController.java
@@ -47,4 +47,22 @@ public class PeopleController {
         return "redirect:/people";
     }
 
+    @GetMapping("/{id}/edit")
+    public String edit(@PathVariable("id") int id, Model model) {
+        model.addAttribute("person", personDao.show(id));
+        return "people/edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("person") Person person, @PathVariable("id") int id) {
+        personDao.update(id, person);
+        return "redirect:/people";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id) {
+        personDao.delete(id);
+        return "redirect:/people";
+    }
+
 }
