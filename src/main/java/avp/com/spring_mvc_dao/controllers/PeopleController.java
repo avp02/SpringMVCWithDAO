cba57fe348ee9d171c04a605d71d9f1a35bf9c69
avp02 <vpetranovskij@gmail.com>
2021-09-20 20:41:58 +0300

    create CRUD application method CREATE

--- src/main/java/avp/com/spring_mvc_dao/controllers/PeopleController.java
+++ src/main/java/avp/com/spring_mvc_dao/controllers/PeopleController.java
@@ -1,12 +1,11 @@
 package avp.com.spring_mvc_dao.controllers;
 
 import avp.com.spring_mvc_dao.dao.PersonDao;
+import avp.com.spring_mvc_dao.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/people")
@@ -30,4 +29,22 @@ public class PeopleController {
         model.addAttribute("person", personDao.show(id));
         return "people/show";
     }
+
+//    @GetMapping("/new") // можно так
+//    public String newPerson(Model model) {
+//        model.addAttribute("person", new Person());
+//        return "people/new";
+//    }
+
+    @GetMapping("/new") // меньше кода
+    public String newPerson(@ModelAttribute("person") Person person) {
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String createPerson(@ModelAttribute("person") Person person) {
+        personDao.save(person);
+        return "redirect:/people";
+    }
+
 }
